b24104a2fe6affd384496ab75f0f835ab59eb492
ArunKumar237 <arunchary237@gmail.com>
2022-12-28 17:48:18 +0530

    commited

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 import requests
 import dill
 import os
+import webbrowser
+
 
 
 def fetch_poster(movie_id):
@@ -38,55 +40,63 @@ def recommend(movie):
     return recommended_movie_names,recommended_movie_posters, detail
 
 
+
 st.header('Movie Recommender System')
-with open(os.path.join(os.getcwd(), "movie_list.pkl"), "rb") as file_obj:
-    movies = dill.load(file_obj)
+with st.container():
+    with open(os.path.join(os.getcwd(), "movie_list.pkl"), "rb") as file_obj:
+        movies = dill.load(file_obj)
 
-with open(os.path.join(os.getcwd(), "similarity.pkl"), "rb") as file_obj:
-    similarity = dill.load(file_obj)
+    with open(os.path.join(os.getcwd(), "similarity.pkl"), "rb") as file_obj:
+        similarity = dill.load(file_obj)
 
-movie_list = movies['title'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
+    movie_list = movies['title'].values
+    selected_movie = st.selectbox(
+        "Type or select a movie from the dropdown",
+        movie_list
+    )
 
-detail = 'None'
+    detail = 'None'
 
-if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters, detail = recommend(selected_movie)
+    if st.button('Show Recommendation'):
+        recommended_movie_names,recommended_movie_posters, detail = recommend(selected_movie)
 
-    col1, col2 = st.columns([1,3])
-    with col1:
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.markdown(detail)
+        col1, col2 = st.columns([1,3])
+        with col1:
+            st.image(recommended_movie_posters[0])
+        with col2:
+            st.markdown(detail)
 
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.markdown(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
-    with col2:
-        st.markdown(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.markdown(recommended_movie_names[1])
+            st.image(recommended_movie_posters[1])
+        with col2:
+            st.markdown(recommended_movie_names[2])
+            st.image(recommended_movie_posters[2])
 
-    with col3:
-        st.markdown(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
+        with col3:
+            st.markdown(recommended_movie_names[3])
+            st.image(recommended_movie_posters[3])
 
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.markdown(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
-    with col2:
-        st.markdown(recommended_movie_names[5])
-        st.image(recommended_movie_posters[5])
-    with col3:
-        st.markdown(recommended_movie_names[6])
-        st.image(recommended_movie_posters[6])
-    
-
-
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.markdown(recommended_movie_names[4])
+            st.image(recommended_movie_posters[4])
+        with col2:
+            st.markdown(recommended_movie_names[5])
+            st.image(recommended_movie_posters[5])
+        with col3:
+            st.markdown(recommended_movie_names[6])
+            st.image(recommended_movie_posters[6])
 
 
 
+
+
+col1, col2 = st.columns(2)
+with col1:
+    if st.button('Git hub link'):
+        webbrowser.open_new_tab(url='https://github.com/ArunKumar237/movie-recommendation')
+with col2:
+    if st.button('linkedin link'):
+        webbrowser.open_new_tab(url='www.linkedin.com/in/panuganti-arun-kumar')
